a97394265a314c55449a0dbddc89e26bbba3505f
GerivanJose <105976240+GerivanJose@users.noreply.github.com>
2022-11-17 19:02:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos2")
+console.log("Rodando o sistema de gerenciamento de jogos3")
+console.log("Rodando o sistema de gerenciamento de jogos4")
+
+
