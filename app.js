bfa2c2b06c4f2210563a480fdb2546ca9964a381
GerivanJose <105976240+GerivanJose@users.noreply.github.com>
2022-11-17 19:02:58 -0300

    Update app.js
    Adicionando 4 linhas

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Rodando o sistema de gerenciamento de jogos2")
 console.log("Rodando o sistema de gerenciamento de jogos3")
 console.log("Rodando o sistema de gerenciamento de jogos4")
-
+console.log("Rodando o sistema de gerenciamento de jogos5")
 
