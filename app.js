bcfbe0de8b6c74b60d159f55d2f5e57655bd0b76
Gerivan Jose Pereira De Araujo <Gerivan.P.Araujo@grupoboticario.com.br>
2022-11-17 19:06:51 -0300

    adicionando um consolelog

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ console.log("Rodando o sistema de gerenciamento de jogos2")
 console.log("Rodando o sistema de gerenciamento de jogos3")
 console.log("Rodando o sistema de gerenciamento de jogos4")
 console.log("Rodando o sistema de gerenciamento de jogos5")
-
+console.log("Enviando para o github")
